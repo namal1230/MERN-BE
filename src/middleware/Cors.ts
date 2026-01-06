7d3f343c763d30a83a1e08ad52e202a6e45726e4
Namal Dilmith <namaldilmith2@gmail.com>
2026-01-06 14:36:51 +0530

    deploy in railway10

--- src/middleware/Cors.ts
+++ src/middleware/Cors.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 const CorsPolicy =  ((req:Request, res:Response, next:NextFunction) => {
   res.setHeader("Access-Control-Allow-Origin", "https://smart-blog-dev.vercel.app");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
